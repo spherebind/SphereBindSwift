07b9215fdbf231915311e0fdf78659cab33b8ffa
Kevin Morales <kfmorales94@gmail.com>
2024-11-28 23:10:00 -0500

    Project Init

---
+++ SphereBindSwift/SphereBindSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphereBindSwiftApp.swift
+//  SphereBindSwift
+//
+//  Created by Kevinho Morales on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SphereBindSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
